2cc6e6fbe8e0db0c22d6a07f74b859edf8a210f2
Sakamoto Natsumi <sky0skier@gmail.com>
2022-04-21 13:57:58 +0900

    キャッシュ消去

--- mysite/attendance/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import LabAttendanceTb
-from .models import LabFingerprintTb
-# Register your models here.
-# admin画面
-admin.site.register(LabAttendanceTb)
-admin.site.register(LabFingerprintTb)
